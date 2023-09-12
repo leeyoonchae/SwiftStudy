3dbb01efa489fc58cc704dec78c97e18260a62bf
leeyoonchae <130755399+leeyoonchae@users.noreply.github.com>
2023-09-12 21:31:34 +0900

    test

---
+++ EnumView/EnumView/EnumViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnumViewApp.swift
+//  EnumView
+//
+//  Created by dgsw8th61 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct EnumViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
